e0a465bd417fac04c50f5d5e508d3c58d93e2d87
Yudha <yudhasanggrama@gmail.com>
2026-01-06 20:48:33 +0700

    chores:fixing status code

--- src/controllers/product-controllers.ts
+++ src/controllers/product-controllers.ts
@@ -28,7 +28,7 @@ export const createProducts = async (req:Request, res:Response)=> {
         const product = await prisma.product.create({
             data:{name,price: parseFloat(price)}
         })
-        res.status(201).json({message: "Product successfully added", data: product})
+        res.status(201).json({message: "Product successfully created", data: product})
     } catch (error) {
         res.status(404).json({error:"Failed to create product"})
     }
@@ -42,8 +42,8 @@ export const editProduct = async(req:Request, res:Response)=> {
         const product = await prisma.product.update({
             where: {id}, 
             data:{
-                ...(name && {name}),
-                ...(price && {price: parseFloat(price)})
+                name,
+                price : parseFloat(price)
             }
         })
 
@@ -58,7 +58,7 @@ export const deleteProduct = async(req:Request, res:Response) => {
     try {
         const id = parseInt(req.params.id)
         const deleteProducts = await prisma.product.delete({where : {id} })
-        res.status(201).json({message: "Product deleted", data:deleteProducts})
+        res.status(200).json({message: "Product deleted", data:deleteProducts})
     } catch (error) {
         res.status(404).json({error:"Failed to delete product"})
     }
